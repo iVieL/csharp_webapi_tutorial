ce607ed7b193498fbe9348321bc53ed64f020f87
Victor Lutin <victorl@autobytel.com>
2019-08-06 12:09:29 -0600

    initial commit

---
+++ TodoApi/DynamicControllers/IsAliveController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.DynamicControllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class IsAliveController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "OK";
+        }
+    }
+}
---
+++ TodoApi/DynamicControllers/IsAliveUrlRewritingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TodoApi.DynamicControllers
+{
+    public class IsAliveUrlRewritingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _overridedRoot;
+
+        public IsAliveUrlRewritingMiddleware(RequestDelegate next, string parent)
+        {
+            _next = next;
+            _overridedRoot = parent;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var path = context.Request.Path.ToUriComponent();
+            if (PathIsAliveRequest(path))
+            {
+                context.Request.Path = "/isalive";
+            }
+
+            var task = _next.Invoke(context);
+            task.Wait();
+            return task;
+        }
+
+        private bool PathIsAliveRequest(string path)
+        {
+            return path.ToLower().Equals($"/{_overridedRoot}/isalive");
+        }
+    }
+}
